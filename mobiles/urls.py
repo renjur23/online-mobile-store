abdf2064783a1d61290ced84628a747dc2d73143
renjur23 <renjurenjithrajendran@gmail.com>
2024-01-31 09:47:46 +0530

    added detail page for mobiles

--- mobiles/urls.py
+++ mobiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MobileListView, SearchResultsListView
+from .views import MobileListView, MobileDetailView, SearchResultsListView
 
 
 urlpatterns = [
     path("", MobileListView.as_view(), name="list"),
+    path("<int:pk>/", MobileDetailView.as_view(), name="detail"),
     path("search/", SearchResultsListView.as_view(), name="search_results"),
 ]
